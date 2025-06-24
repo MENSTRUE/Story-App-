84822a3bab57afa21d3c31d3eae17b10d0956840
MENSTRUE <menstruesalvation@gmail.com>
2025-06-24 10:05:39 +0700

    day #1

---
+++ app/src/main/java/com/dicoding/storyapp/view/main/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.dicoding.storyapp.view.main
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.storyapp.databinding.ActivityMainBinding
+import com.dicoding.storyapp.view.ViewModelFactory
+import com.dicoding.storyapp.view.welcome.WelcomeActivity
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MainViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.getSession().observe(this) { user ->
+            if (!user.isLogin) {
+                startActivity(Intent(this, WelcomeActivity::class.java))
+                finish()
+            }
+        }
+
+        setupView()
+        setupAction()
+        playAnimation()
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val logodicoding =
+            ObjectAnimator.ofFloat(binding.imageView, View.ALPHA, 1f).apply { duration= 500 }
+        val Name = ObjectAnimator.ofFloat(binding.nameTextView, View.ALPHA, 1f).apply { duration= 500 }
+        val message = ObjectAnimator.ofFloat(binding.messageTextView, View.ALPHA, 1f). apply { duration= 500 }
+        val btnLogout = ObjectAnimator.ofFloat(binding.logoutButton, View.ALPHA, 1f). apply { duration= 500 }
+
+        // Daftar view yang akan dianimasikan ALPHA
+        val alphaAnimations = listOf(
+            binding.imageView,
+            binding.nameTextView,
+            binding.messageTextView,
+            binding.logoutButton
+
+        ).map { view ->
+            ObjectAnimator.ofFloat(view, View.ALPHA, 1f).apply {
+                duration = 100
+            }
+        }
+
+        AnimatorSet().apply {
+            playSequentially(
+
+                logodicoding,
+                Name,
+                message,
+                btnLogout
+
+            )
+            startDelay = 100
+            start()
+        }
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.logoutButton.setOnClickListener {
+            viewModel.logout()
+        }
+    }
+
+}
